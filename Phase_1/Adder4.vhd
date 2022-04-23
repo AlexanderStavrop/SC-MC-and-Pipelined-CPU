2aa913e0d9c18c21d75ef51b6a9e4d96644b1a35
Alexander Stavrop <alexanderstavrop@gmail.com>
2022-04-23 14:44:00 +0300

    I hate 8 tabs

--- Phase_1/Adder4.vhd
+++ Phase_1/Adder4.vhd
@@ -4,15 +4,15 @@ use ieee.std_logic_unsigned.all;
 
 -- Creating the entity of Adder4
 entity Adder4 is
-    Port ( 	DataIn 	: in  std_logic_vector(31 downto 0);
-	        DataOut : out std_logic_vector(31 downto 0)
-	);
+    Port (  DataIn     : in  std_logic_vector(31 downto 0);
+                DataOut  : out std_logic_vector(31 downto 0)
+    );
 end Adder4;
 
 architecture Behavioral of Adder4 is
 
 -------------------------------------------------- Main Functions --------------------------------------------------                                                                                        
 begin
-	DataOut <= DataIn + 4 after 10ns;		-- Adding 4 to the input value
+    DataOut <= DataIn + 4 after 10ns;        -- Adding 4 to the input value
 end Behavioral;
 
