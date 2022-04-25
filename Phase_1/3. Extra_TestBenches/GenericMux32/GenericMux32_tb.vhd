9617289202267bbddb35e2313fb6bc22b3eca457
Alexander Stavrop <alexanderstavrop@gmail.com>
2022-04-25 15:02:22 +0300

    Fixed some indentation issues

--- Phase_1/3. Extra_TestBenches/GenericMux32/GenericMux32_tb.vhd
+++ Phase_1/3. Extra_TestBenches/GenericMux32/GenericMux32_tb.vhd
@@ -8,14 +8,14 @@ END GenericMux32_tb;
 ARCHITECTURE behavior OF GenericMux32_tb IS
 	-- Creating the component of GenericMux
 	COMPONENT GenericMux32
-		PORT ( DataIn  	: in MuxMatrix32;
-					DataSel 	: in std_logic_vector(4 downto 0);
-					DataOut : out std_logic_vector(31 downto 0)
+		PORT ( DataIn  : in MuxMatrix32;
+			   DataSel : in std_logic_vector(4 downto 0);
+			   DataOut : out std_logic_vector(31 downto 0)
       );
 	END COMPONENT;
     
    -- Inputs Signal
-   signal DataIn   : MuxMatrix32(3 downto 0) := (others => (others => '0'));
+   signal DataIn  : MuxMatrix32(3 downto 0) := (others => (others => '0'));
    signal DataSel : std_logic_vector(4 downto 0) := (others => '0');
 
  	-- Outputs Signal
@@ -24,9 +24,9 @@ ARCHITECTURE behavior OF GenericMux32_tb IS
 BEGIN
 	-- Declaring the Register32 port map
 	uut: GenericMux32 
-		PORT MAP ( DataIn     => DataIn,
-							  DataSel   => DataSel,
-							  DataOut  => DataOut
+		PORT MAP ( DataIn  => DataIn,
+				   DataSel => DataSel,
+				   DataOut => DataOut
 		); 
 
 	-- Stimulus process
@@ -40,17 +40,17 @@ BEGIN
 		DataIn(2) <= "00000000000000000000000000000010";  -- Setting the third  input equal to 2
 		DataIn(3) <= "00000000000000000000000000000011";  -- Setting the forth  input equal to 3
 		----------------------------------------------------------------------------------------------------------------------------------------------------- 0ns
-		DataSel <= "00000";										  						 -- Selecting the first input
-		wait for 100 ns;											  							 -- Expecting 0
+		DataSel <= "00000";								  -- Selecting the first input
+		wait for 100 ns;								  -- Expecting 0
 		----------------------------------------------------------------------------------------------------------------------------------------------------- 100ns		
-		DataSel <= "00001";										  						 -- Selecting the second input
-		wait for 100 ns;											  							 -- Expecting 1
+		DataSel <= "00001";								  -- Selecting the second input
+		wait for 100 ns;								  -- Expecting 1
 		----------------------------------------------------------------------------------------------------------------------------------------------------- 200ns		
-		DataSel <= "00010";										  						 -- Selecting the third input
-		wait for 100 ns;											  							 -- Expecting 2
+		DataSel <= "00010";								  -- Selecting the third input
+		wait for 100 ns;								  -- Expecting 2
 		----------------------------------------------------------------------------------------------------------------------------------------------------- 300ns
-		DataSel <= "00011";										  						 -- Selecting the forth input
-		wait for 100 ns;											  							 -- Expecting 3
+		DataSel <= "00011";								  -- Selecting the forth input
+		wait for 100 ns;								  -- Expecting 3
 		----------------------------------------------------------------------------------------------------------------------------------------------------- 400ns
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -61,17 +61,17 @@ BEGIN
 		DataIn(2) <= "10101010101010101010101010101010";  -- Setting the third  input equal to -1431655766
 		DataIn(3) <= "00100101101110001110010110101010";  -- Setting the forth  input equal to 632874410
 		----------------------------------------------------------------------------------------------------------------------------------------------------- 400ns
-		DataSel <= "00000";										  						 -- Selecting the first input
-		wait for 100 ns;											  							 -- Expecting 4134976
+		DataSel <= "00000";								  -- Selecting the first input
+		wait for 100 ns;								  -- Expecting 4134976
 		----------------------------------------------------------------------------------------------------------------------------------------------------- 500ns		
-		DataSel <= "00001";										  						 -- Selecting the second input
-		wait for 100 ns;											  							 -- Expecting -1
+		DataSel <= "00001";								  -- Selecting the second input
+		wait for 100 ns;								  -- Expecting -1
 		----------------------------------------------------------------------------------------------------------------------------------------------------- 600ns		
-		DataSel <= "00010";										  						 -- Selecting the third input
-		wait for 100 ns;											  							 -- Expecting -1431655766
+		DataSel <= "00010";								  -- Selecting the third input
+		wait for 100 ns;								  -- Expecting -1431655766
 		----------------------------------------------------------------------------------------------------------------------------------------------------- 700ns
-		DataSel <= "00011";										  						 -- Selecting the forth input
-		wait for 100 ns;											  							 -- Expecting 632874410
+		DataSel <= "00011";								  -- Selecting the forth input
+		wait for 100 ns;								  -- Expecting 632874410
 		----------------------------------------------------------------------------------------------------------------------------------------------------- 800ns
 		wait; 
 	end process;
