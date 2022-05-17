7172937fce915b21ad7fb8888cadd5e691c539d1
Alexandros Stavropoulos <alexanderstavrop@gmail.com>
2022-05-17 05:35:36 +0300

    Removed Ram module from Processor

--- Phase_4/PROCESSOR_MC/PROCESSOR_MC.vhd
+++ Phase_4/PROCESSOR_MC/PROCESSOR_MC.vhd
@@ -2,7 +2,12 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity PROCESSOR_MC is
-	port (Clk, Reset : in std_logic);
+	port ( Clk, Reset			  : in  std_logic;
+		   MM_RdData 			  : in  std_logic_vector (31 downto 0);
+		   Instruction 			  : in  std_logic_vector (31 downto 0);
+		   MM_WrEn			  	  : out std_logic;
+		   PC, MM_Addr, MM_WrData : out std_logic_vector (31 downto 0)
+	);
 end PROCESSOR_MC;
 
 architecture Behavioral of PROCESSOR_MC is
@@ -45,15 +50,6 @@ architecture Behavioral of PROCESSOR_MC is
 		);
 	end component;
 	
-	component RAM is
-		 port ( clk, data_we 		  								  : in  std_logic;
-				inst_addr, data_addr 								  : in  std_logic_vector(10 downto 0);
-				data_din 				  						      : in  std_logic_vector(31 downto 0);
-				inst_dout				  							  : out std_logic_vector(31 downto 0);
-				data_dout 			  								  : out std_logic_vector(31 downto 0)
-		);
-	end component; 
-	
 	
 	-- Needed signals
 	-- IFSTAGE ---------------------------------------------------------------------------------------------------------------------
@@ -66,10 +62,7 @@ architecture Behavioral of PROCESSOR_MC is
 	signal IN_ALU_Bin_sel, IN_ALU_zero, IN_ALUOut_Reg_WrEn 		 							 : std_logic := '0';
 	signal IN_ALU_func 								 			 							 : std_logic_vector ( 3 downto 0) := (others => '0');
 	-- MEMSTAGE --------------------------------------------------------------------------------------------------------------------
-	signal IN_ByteOp, IN_Mem_WrEn, IN_MM_WrEn, IN_Mem_Reg_WrEn							     : std_logic := '0';
-	signal IN_Instruction, IN_MM_Addr, IN_MM_WrData	 			 	 						 : std_logic_vector (31 downto 0) := (others => '0');
-	-- Ram -------------------------------------------------------------------------------------------------------------------------
-	signal IN_MM_RdData                              			 							 : std_logic_vector (31 downto 0) := (others => '0');
+	signal IN_ByteOp, IN_Mem_WrEn,IN_Mem_Reg_WrEn							     			 : std_logic := '0';
 
 
 -------------------------------------------------- Main Functions --------------------------------------------------                                                                                        
@@ -79,7 +72,7 @@ begin
 		port map ( Clk 		       => Clk,
 				   Reset           => Reset,
 				   ALU_zero 	   => IN_ALU_zero,
-				   Instruction	   => IN_Instruction,
+				   Instruction	   => Instruction,
 				   PC_sel          => IN_PC_sel, 
 				   PC_LdEn         => IN_PC_LdEn,
 				   IR_WrEn         => IN_IR_WrEn,
@@ -103,7 +96,7 @@ begin
 				   Rst     		   => Reset,
 				   PC_sel  		   => IN_PC_sel,
 				   PC_LdEn 		   => IN_PC_LdEn,
-				   PC_out	 	   => IN_PC_out,
+				   PC_out	 	   => PC,
 				   IR_WrEn         => IN_IR_WrEn,
 				   RF_WrData_sel   => IN_RF_WrData_sel, 
 				   RF_B_sel		   => IN_RF_B_sel,
@@ -111,7 +104,7 @@ begin
 				   ImmExt_s        => IN_ImmExt_s,
 				   RF_A_WrEn       => IN_RF_A_WrEn,
 				   RF_B_WrEn       => IN_RF_B_WrEn,
-				   Instruction     => IN_Instruction,
+				   Instruction     => Instruction,
 				   ALU_Bin_sel     => IN_ALU_Bin_sel,
 				   ALU_func        => IN_ALU_func,
 				   ALUOut_Reg_WrEn => IN_ALUOut_Reg_WrEn,
@@ -120,23 +113,13 @@ begin
 				   ALU_ovf         => open,
 				   ByteOp          => IN_ByteOp, 
 				   Mem_WrEn        => IN_Mem_WrEn,
-				   MM_RdData       => IN_MM_RdData,
-				   MM_WrEn         => IN_MM_WrEn,
-				   MM_Addr         => IN_MM_Addr,
-				   MM_WrData       => IN_MM_WrData,
+				   MM_RdData       => MM_RdData,
+				   MM_WrEn         => MM_WrEn,
+				   MM_Addr         => MM_Addr,
+				   MM_WrData       => MM_WrData,
 				   Mem_Reg_WrEn    => IN_Mem_Reg_WrEn
 		);
-	
--------------------------------------------------------- Ram --------------------------------------------------------
-	Ram_module:Ram
-		port map ( clk       	   => Clk,
-				   data_we   	   => IN_MM_WrEn,
-				   inst_addr 	   => IN_PC_out(12 downto 2),
-				   data_addr	   => IN_MM_Addr(10 downto 0),
-				   data_din  	   => IN_MM_WrData,
-				   inst_dout 	   => IN_Instruction,
-				   data_dout 	   => IN_MM_RdData 
-		);
+
 
 end Behavioral;
 
--- Phase_4/PROCESSOR_MC/PROCESSOR_MC_tb.vhd
+++ Phase_4/PROCESSOR_MC/PROCESSOR_MC_tb.vhd
@@ -7,25 +7,60 @@ END PROCESSOR_MC_tb;
 ARCHITECTURE behavior OF PROCESSOR_MC_tb IS 
  
 	component PROCESSOR_MC
-		port( Clk, Reset : in  std_logic);
+		port ( Clk, Reset			  : in  std_logic;
+			   MM_RdData, Instruction : in  std_logic_vector (31 downto 0);
+			   MM_WrEn			  	  : out std_logic;
+			   PC, MM_Addr, MM_WrData : out std_logic_vector (31 downto 0)
+		);
     end component;
     
-	--Inputs
+	component RAM is
+		 port ( Clk, data_we 		  : in  std_logic;
+				inst_addr, data_addr  : in  std_logic_vector(10 downto 0);
+				data_din 			  : in  std_logic_vector(31 downto 0);
+				inst_dout			  : out std_logic_vector(31 downto 0);
+				data_dout 			  : out std_logic_vector(31 downto 0)
+		);
+	end component; 
+	
+	-- Input/output signals --------------------------------------------------
 	signal Clk 		 	  : std_logic := '0';
 	signal Reset 	 	  : std_logic := '0';
-
+	signal MM_RdData 	  : std_logic_vector (31 downto 0);
+	signal Instruction    : std_logic_vector (31 downto 0);
+	signal MM_WrEn		  : std_logic;
+	signal MM_Addr	 	  : std_logic_vector (31 downto 0);
+	signal MM_WrData 	  : std_logic_vector (31 downto 0);
+	signal PC			  : std_logic_vector (31 downto 0);
+	
     -- Clock period definitions
 	constant clk_period   : time := 30 ns; 			 
 	signal stop_the_clock : boolean;
  
 BEGIN
  
-	-- Instantiate the Unit Under Test (UUT)
 	uut: PROCESSOR_MC 
-		PORT MAP ( Clk 		 => Clk,
-				   Reset  	 => Reset
+		PORT MAP ( Clk 		   => Clk,
+				   Reset  	   => Reset,
+				   PC		   => PC,
+				   Instruction => Instruction,
+				   MM_RdData   => MM_RdData,
+				   MM_WrEn	   => MM_WrEn,
+				   MM_Addr 	   => MM_Addr,
+				   MM_WrData   => MM_WrData
 		);
 
+		
+	Ram_Module: RAM
+		port map ( Clk         => Clk,
+				   data_we     => MM_WrEn,
+				   inst_addr   => PC(12 downto 2),
+				   data_addr   => MM_Addr(10 downto 0),
+				   data_din    => MM_WrData,
+				   inst_dout   => Instruction,
+				   data_dout   => MM_RdData 
+		);
+		
 	-- Clock process definitions
    clocking: process
 	  begin
