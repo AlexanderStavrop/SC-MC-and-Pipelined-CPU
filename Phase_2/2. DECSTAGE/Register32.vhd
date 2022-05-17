699a2c8d394ad7d72d8562d4695c3ea194260c41
Alexandros Stavropoulos <alexanderstavrop@gmail.com>
2022-05-17 19:47:44 +0300

    Updated a comment

--- Phase_2/2. DECSTAGE/Register32.vhd
+++ Phase_2/2. DECSTAGE/Register32.vhd
@@ -21,10 +21,10 @@ begin
 				if (RST = '1') then					-- Checking if the reset button is pressed.
 					IN_out <= (others => '0');		-- Updating the output to zeros immediately.
 				elsif (WE = '1') then 				-- Checking if we also have write enable pressed.
-					IN_out <= DataIn;				-- Updating the Output from the corresponding variable after 10ns.
+					IN_out <= DataIn;				-- Updating the Output from the corresponding variable.
 				end if;
 			end if;
-		DataOut <= IN_out after 10ns;				-- Setting the DataOut with the corresponding value and adding 12ns of delay.
+		DataOut <= IN_out after 10ns;				-- Setting the DataOut with the corresponding value and adding 10ns of delay.
 	end process;
 	
 end Behavioral;
